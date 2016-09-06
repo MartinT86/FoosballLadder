03aff80ffecaf6c27ab03cf6e5edfb2eafe033c7
m <m@m.com>
2016-09-06 20:30:31 +0100

    initial

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!! Set up
+         </div>
+      );
+   }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+
+var outputPath = path.resolve(__dirname, 'src/build');
+var entryPath = path.resolve(__dirname, 'src/app/main.js');
+
+var config = {
+   entry: entryPath,
+	
+   output: {
+      path:outputPath,
+      filename: 'index.js',
+      publicPath: 'src/build'
+   },
+	
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
