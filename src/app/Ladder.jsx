98121a26d35cb2729cd5096a44584af6d9966405
m <martin.tierney86@hotmail.co.uk>
2016-09-06 23:10:27 +0100

    ladder now has state

--- src/app/Ladder.jsx
+++ src/app/Ladder.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import Player from './Player.jsx'
 
 class Ladder extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            players : ['Player One', 'Player Two']
+        }
+    }
+
    render() {
       return (
          <div>
             Ladder Container
-            <Player/>
+            {this.state.players.map(function(player, i){
+                return (
+                    <Player name={player} key={i}/>
+                )
+            })}
          </div>
       );
    }
