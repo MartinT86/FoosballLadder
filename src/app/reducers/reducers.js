ea2980d936c1d3e6d938ce7eef2eecfa2041946e
m <martin.tierney86@hotmail.co.uk>
2016-09-11 00:17:55 +0100

    reducer added

---
+++ src/app/reducers/reducers.js
@@ -0,0 +1,16 @@
+import { ADD_PLAYER } from './actions/actions.js'
+
+const initialState = {
+  players : ['Player One', 'Player Two', 'Player Three']
+}
+
+function foosballApp(state = initialState, action) {
+  switch (action.type) {
+    case ADD_PLAYER:
+      return {...state, players: [...state.players,player.player]} 
+    default:
+      return state
+  }
+}
+
+export default foosballApp
